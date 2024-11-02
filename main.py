b717fa633587f3913bff695b1e67d91a924435ed
marbel <othmane.xx90@gmail.com>
2024-11-02 19:27:53 +0100

    linked list queue

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.teal = None
 
     def __iter__(self):
         node = self.head
@@ -96,17 +97,32 @@ class LinkedList:
             yield node
             node = node.next
 
+#    def add_to_tail(self, node):
+#        if self.head is None:
+#            self.head = node
+#        else:
+#            last = None
+#            index = self.head
+#            while index is not None:
+#                last = index
+#                index = index.next 
+#            last.next = node
+
+    def add_to_head(self, node):
+        if self.head is None:
+            self.tail = node
+        node.set_next(self.head)
+        self.head = node
+
     def add_to_tail(self, node):
         if self.head is None:
             self.head = node
+            self.tail = node
         else:
-            last = None
-            index = self.head
-            while index is not None:
-                last = index
-                index = index.next 
-            last.next = node
-    # don't touch below this line
+            self.tail.set_next(node)
+            self.tail = node 
+        
+
 
     def __repr__(self):
         nodes = []
